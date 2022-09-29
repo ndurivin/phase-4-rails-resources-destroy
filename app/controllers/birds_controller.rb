fc095a5c80512c8704e4fb2ba6aebad21868ee84
Vinn Nduri <ndurivo@gmail.com>
2022-09-29 12:01:26 +0300

    Done

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -44,6 +44,20 @@ class BirdsController < ApplicationController
     end
   end
 
+   # DELETE /birds
+
+   def destroy 
+    bird = Bird.find_by(id: params[:id])
+    if bird 
+      bird.destroy 
+      render json: {}
+      head :no_content
+
+    else
+      render json: {error: "bird not found"}, status: :not_found
+    end
+   end
+
   private
 
   def bird_params
